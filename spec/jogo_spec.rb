c0a047e65c67d70959f584616abc44c7f6236125
Atelie <atelie@iMac-de-Vagas.local>
2019-09-23 10:58:49 -0300

    merge

--- spec/jogo_spec.rb
+++ spec/jogo_spec.rb
@@ -1,9 +1,9 @@
 require './src/jogo'
 
 describe 'Tela inicial' do
-  jogo = Jogo.new
+  xit 'deve ter o pacman no centro' do
+    jogo = Jogo.new
 
-  it 'deve ter o pacman no centro' do
     expected = "     \n"+
                "     \n"+
                "  c  \n"+
@@ -13,7 +13,7 @@ describe 'Tela inicial' do
     expect(jogo.tela).to eq expected
   end
 
-  xit 'o pacman deve andar para direita' do
+  xit 'deve o pacman andar para direita' do
     jogo.direita
 
     expected = "     \n"+
@@ -22,6 +22,36 @@ describe 'Tela inicial' do
                "     \n"+
                "     \n"
 
+    expect(jogo.tela).to eq expected
+  end
+
+  xit 'deve o pacman andar para esquerda' do
+    jogo.esquerda
+    expected = "     \n"+
+               "     \n"+
+               " c   \n"+
+               "     \n"+
+               "     \n"
+    expect(jogo.tela).to eq expected
+  end
+
+  xit 'deve o pacman andar para cima' do
+    jogo.cima
+    expected = "     \n"+
+               "  c  \n"+
+               "     \n"+
+               "     \n"+
+               "     \n"
+    expect(jogo.tela).to eq expected
+  end
+
+  xit 'deve o pacman andar para baixo' do
+    jogo.baixo
+    expected = "     \n"+
+               "     \n"+
+               "     \n"+
+               "  c  \n"+
+               "     \n"
     expect(jogo.tela).to eq expected
   end
 end
