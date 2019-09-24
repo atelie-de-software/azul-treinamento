867a2bca16b1bb2281daa886f05fd798d74a3b9d
Atelie <atelie@iMac-de-Vagas.local>
2019-09-24 11:19:55 -0300

    oo

--- src/jogo.rb
+++ src/jogo.rb
@@ -1,49 +1,72 @@
 class Jogo
   def initialize(pac_position_x = 2, pac_position_y = 2)
+    @tela_calculada = ""
     @pac_position_x = pac_position_x
     @pac_position_y = pac_position_y
+    @ball_1 = [0,0]
+    @ball_1_live = true
+    @ball_2 = [4,4]
+    @ball_2_live = true
+  end
+
+  def tela
+    @tela_calculada == "" if calcular_tela
+    @tela_calculada
   end
 
   def sobe
     @pac_position_y -= 1 if @pac_position_y - 1 >= 0
+    calcular_tela
   end
 
   def desce
     @pac_position_y += 1 if @pac_position_y + 1 < 5
+    calcular_tela
   end
 
   def esquerda
     @pac_position_x -= 1 if @pac_position_x - 1 >= 0
+    calcular_tela
   end
 
   def direita
     @pac_position_x += 1 if @pac_position_x + 1 < 5
+    calcular_tela
   end
 
-  def tiro; end
-
-  def tela
-    tela_default = ""
+  def calcular_tela
+    @tela_calculada = ""
 
     5.times do |y|
       5.times do |x|
         if [[x,y]].include? [@pac_position_x, @pac_position_y]
-          tela_default += (x == 4 ? "c\n" : "c")
+          @tela_calculada += (x == 4 ? "c\n" : "c")
           next
         end
 
-        if [[0,0],[4,4]].include? [x,y]
-          tela_default += (x == 4 ? "*\n" : "*")
-          next
+        @ball_1_live = false if @ball_1 == [@pac_position_x, @pac_position_y]
+        @ball_2_live = false if @ball_2 == [@pac_position_x, @pac_position_y]
+
+        if @ball_1 ==  [x,y]
+          if @ball_1_live
+            @tela_calculada += (x == 4 ? "*\n" : "*")
+            next
+          end
         end
 
-        tela_default += " "
-        tela_default += "\n" if x == 4
+        if @ball_2 ==  [x,y]
+          if @ball_2_live
+            @tela_calculada += (x == 4 ? "*\n" : "*")
+            next
+          end
+        end
+
+        @tela_calculada += " "
+        @tela_calculada += "\n" if x == 4
       end
     end
-
-    tela_default
   end
 
-  def tick; end
+  def tick
+  end
 end
