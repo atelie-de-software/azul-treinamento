cfc528a48e17756b5d7c5229403c133efed896d2
William <william@webgoal.com.br>
2019-09-24 16:38:27 -0300

    modo grafico

--- main.rb
+++ main.rb
@@ -1,21 +1,78 @@
 require 'dispel'
 require './src/jogo.rb'
+require 'ruby2d'
+
+# Set the window size
+set width: 250, height: 250
+set background: 'black'
 
 jogo = Jogo.new
 
-Dispel::Screen.open do |screen|
-  Dispel::Keyboard.output timeout: 0.5 do |key|
+
+p "Deseja iniciar o modo grafico? s/n"
+resposta = gets[0]
+
+if resposta == 's'
+  on :key_down do |event|
+    case event.key
+      when 'left'
+        jogo.esquerda
+      when 'right'
+        jogo.direita
+      when 'up'
+        jogo.sobe
+      when 'down'
+        jogo.desce
+    end
+  end
+
+  update do
+    clear
     jogo.tick
-    jogo.esquerda if key == :left
-    jogo.direita  if key == :right
-    jogo.sobe     if key == :up
-    jogo.desce    if key == :down
+    pixel = 0
+    jogo.tela.split("\n")[0].each_char.with_index { |c, index|
+      Image.new('images/food.png', x: index * 50, y: 0)  if c == "*"
+      Image.new('images/p_right.png', x: index * 50, y: 0)  if c == "c"
+    }
 
-    screen.draw " PACMAN\n" + jogo.tela
+    jogo.tela.split("\n")[1].each_char.with_index { |c, index|
+      Image.new('images/food.png', x: index * 50, y: 50)  if c == "*"
+      Image.new('images/p_right.png', x: index * 50, y: 50)  if c == "c"
+    }
 
-    next          unless key
-    next          if     key == :timeout
-    exit(true)    if     key == :"Ctrl+c"
+    jogo.tela.split("\n")[2].each_char.with_index { |c, index|
+      Image.new('images/food.png', x: index * 50, y: 100)  if c == "*"
+      Image.new('images/p_right.png', x: index * 50, y: 100)  if c == "c"
+    }
 
+    jogo.tela.split("\n")[3].each_char.with_index { |c, index|
+      Image.new('images/food.png', x: index * 50, y: 150)  if c == "*"
+      Image.new('images/p_right.png', x: index * 50, y: 150)  if c == "c"
+    }
+
+    jogo.tela.split("\n")[4].each_char.with_index { |c, index|
+      Image.new('images/food.png', x: index * 50, y: 200)  if c == "*"
+      Image.new('images/p_right.png', x: index * 50, y: 200)  if c == "c"
+    }
+  end
+
+  show
+
+else
+  Dispel::Screen.open do |screen|
+    Dispel::Keyboard.output timeout: 0.5 do |key|
+      jogo.tick
+      jogo.esquerda if key == :left
+      jogo.direita  if key == :right
+      jogo.sobe     if key == :up
+      jogo.desce    if key == :down
+
+      screen.draw " PACMAN\n" + jogo.tela
+
+      next          unless key
+      next          if     key == :timeout
+      exit(true)    if     key == :"Ctrl+c"
+
+    end
   end
 end
