8c7b24577477dcffe5b9baab24c248c9ba01f383
mateus andrade <mateus.souza@webgoal.com.br>
2019-09-24 09:31:56 -0300

    Refatora main

--- main.rb
+++ main.rb
@@ -5,16 +5,12 @@ jogo = Jogo.new
 
 Dispel::Screen.open do |screen|
   Dispel::Keyboard.output timeout: 0.5 do |key|
-    jogo.esquerda if key == :left
-    jogo.direita  if key == :right
-    jogo.sobe     if key == :up
-    jogo.desce    if key == :down
+    jogo.tecla(key)
 
     screen.draw jogo.tela
 
     next          unless key
     next          if     key == :timeout
-
-    exit(true)    if key == :"Ctrl+c"
+    exit(true)    if     key == :"Ctrl+c"
   end
 end
